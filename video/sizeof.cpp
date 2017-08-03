d768c2bd713976431afa343b3353f639a115e2d6
indjec <ind.jec@gmail.com>
2017-08-03 17:34:54 +0530

    bytes

---
+++ video/sizeof.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"int -->"<<sizeof(int)<<endl;
+    cout<<"short int -->"<<sizeof( short int)<<endl;
+    cout<<"float -->"<<sizeof(float)<<endl;
+    cout<<"char -->"<<sizeof(char)<<endl;
+}
